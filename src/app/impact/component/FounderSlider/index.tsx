8f3134998a1a8387007c06200f29e02efd76032f
vigneshdfc <vigneshdfc02@gmail.com>
2023-04-18 11:21:08 +0530

    chore:push

--- src/app/impact/component/FounderSlider/index.tsx
+++ src/app/impact/component/FounderSlider/index.tsx
@@ -18,26 +18,46 @@ const Founder = () => {
     arrows: false,
   };
   return (
-    <Slider {...settings}>
-      {Data.map((f, index) => {
-        return (
-          <div className={styles.slider} key={index}>
-            <h5>{f.heading}</h5>
-            <div className={styles.founder}>
-              <Image src={f.image} alt="" />
-              <div>
-                <h4>{f.name}</h4>
-                <h6>{f.position}</h6>
-              </div>
-            </div>
-            <p>{f.discription}</p>
+    // <Slider {...settings}>
+    //   {Data.map((f, index) => {
+    //     return (
+    //       <div className={styles.slider} key={index}>
+    //         <h5>{f.heading}</h5>
+    //         <div className={styles.founder}>
+    //           <Image src={f.image} alt="" />
+    //           <div>
+    //             <h4>{f.name}</h4>
+    //             <h6>{f.position}</h6>
+    //           </div>
+    //         </div>
+    //         <p>{f.discription}</p>
 
-            <Link href={"/webdevelopment"}>
-              <Button varient="primary">Read casestudy</Button>
-            </Link>
-          </div>
-        );
-      })}
+    //         <Link href={"/webdevelopment"}>
+    //           <Button varient="primary">Read casestudy</Button>
+    //         </Link>
+    //       </div>
+    //     );
+    //   })}
+    // </Slider>
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
     </Slider>
   );
 };
